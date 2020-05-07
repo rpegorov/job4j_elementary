c7a0c0286981bd845000a8a91c098c1b789cb786
rpegorov <rostislav.egorov@gmail.com>
2020-05-07 16:38:51 +0300

    Разбор первой программы.[#286457]

---
+++ test/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Egorov Rostislav");
+        System.out.println("29.06.1991");
+
+    }
+
+}
