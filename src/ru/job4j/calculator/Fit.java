903e8240a9bcc5e1e9472514192085cf0eeca16f
rpegorov <rostislav.egorov@gmail.com>
2020-05-11 19:03:32 +0300

    Идеальный вес.[#286438] Исправленный

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,10 +1,12 @@
 package ru.job4j.calculator;
 
 /**
- * @version 1 Калькулятор идеального веса
+ * Калькулятор идеального веса
  * @author rpegorov
- * @since  09.05.2020
+ * @version 1
+ * @since 09.05.2020
  */
+
 public class Fit {
     public static double manWeight(double height) {
         return (height - 100) * 1.15;
@@ -21,8 +23,7 @@ public class Fit {
         double woman = Fit.womanWeight(163);
         System.out.println("Идеальный вес для мужчины ростом 181 см составляет " + man + " кг");
         System.out.println("Идеальный вес для женщины ростом 163 см составляет " + woman + " кг");
-        // какие то жирные параметры выходят...
+
     }
-//Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15.
-//Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
+
 }
