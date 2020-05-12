34d1bac034d8cb75595cb54b2a737d3aef95d24c
rpegorov <rostislav.egorov@gmail.com>
2020-05-12 17:00:09 +0300

    Подправил формулу.

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -16,7 +16,7 @@ public class Point {
      * return берет результат из double distance и достает из него квадратный корень
      */
     public static double distance(int x1, int y1, int x2, int y2) {
-        double distance = Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2);
+        double distance = Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2);
         return Math.sqrt(distance);
 
     }
