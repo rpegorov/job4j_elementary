20bb5869afc0449f088e7541183f7452d5012863
rpegorov <rostislav.egorov@gmail.com>
2020-05-12 18:09:46 +0300

    String[#286448]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String separete = " ";
+        int year = 2020;
+        String idea = "I Like Java!" + separete + "But I am a newbie." + separete + year;
+
+
+        System.out.println(idea);
+        // System.out.println(idea + separete + newbie);
+        // System.out.println(idea + separete + year);
+    }
+}
