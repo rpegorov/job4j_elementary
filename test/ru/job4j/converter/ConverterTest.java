75561084c97a1a539bf3e83f58c6e5970da833f3
rpegorov <rostislav.egorov@gmail.com>
2020-05-16 21:39:46 +0300

    Модульные тесты.[#286473] Тест на Converter.

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.converter;
+
+// в данном коде будем использовать код из файлов Assert & Test
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Тестирование класса Converter
+ * @author rpegorov
+ * @since 16.05.2020
+ */
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;  // вводные данные (сколько рублей)
+        int expected = 2;  // ожидаемый результат
+        int out = Converter.rubleToEuro(in);  // рузультат работы программы
+
+        // Assert.assertEquals - метод сранивает параметры expected и out
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 200;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
