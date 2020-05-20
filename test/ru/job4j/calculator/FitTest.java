8640c973901bb28e567c0967336d1e0a483f5b0a
rpegorov <rostislav.egorov@gmail.com>
2020-05-20 10:50:09 +0300

    Тесты для идеальный вес[159458#286474]. Добавил комментарии.

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -15,6 +15,10 @@ public class FitTest {
         double in = 181;
         double expected = 93.14;
         double out = Fit.manWeight(in);
+
+        // Для сравнения результатов с типом double необходимо использовать метод с тремя параметрами.
+        // Число 0.01 используется для проверки того,
+        // что результат теста будет в диапазоне [expected - 0.01, expected + 0.01].
         Assert.assertEquals(expected, out, 0.01);
 
     }
