2ea6a6eacea92cf9a0e7af669ef0a5e4ee2132e3
rpegorov <rostislav.egorov@gmail.com>
2020-05-21 14:25:35 +0300

    Операторы ветвлений[3611#286408]. Постановка условий.

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+import javax.swing.*;
+
+/**
+ * Операции сравнения. Уведомление при делении на 0 или < 0.
+ * @author rpegorov
+ * @version 1
+ * @since 21.05.2020
+ */
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number == 0) {  // если равно 0
+            System.out.println("Could not div by 0.");  // ввывод данного сообщения
+
+        } else if (number <= 0) {  // если меньше 0
+            System.out.println("This is negative numbers.");  // вывод данного сообщения
+        }
+    }
+}
+
+
+
+
+
