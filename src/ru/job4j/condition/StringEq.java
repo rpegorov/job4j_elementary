87a47f41705f45d52eb4f53bc2cee147ae9b52ca
rpegorov <rostislav.egorov@gmail.com>
2020-05-21 19:18:07 +0300

    Операторы сравнения в String.[237406#286404]

---
+++ src/ru/job4j/condition/StringEq.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class StringEq {
+    public static boolean check(String login) {
+        String root = "root";
+        return root.equals(login);  // boolean принимает значение true\false
+    }
+
+    public static void main(String[] args) {
+        String your = "your_name";
+        boolean userHasAccess = StringEq.check(your);
+        System.out.println(userHasAccess);
+    }
+}
+
+
