4bfe556e5a87711f8df1c50f45a0824900999fca
rpegorov <rostislav.egorov@gmail.com>
2020-05-25 17:32:27 +0300

    Тернарное сравнение[189#286406]. Упростил, теперь 1 строка

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean max = left >= right;
-        return max ? left : right;
+       return left > right ? left : right;
     }
 }
