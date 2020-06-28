119e23c319d14d13b3e65b838e6b7d1e4c56b0b2
rpegorov <rostislav.egorov@gmail.com>
2020-06-28 19:23:16 +0300

    Генерация циклов в IDEA - iter, itar.[279134#286494]

--- src/ru/job4j/live/Main.java
+++ src/ru/job4j/live/Main.java
@@ -1,10 +1,9 @@
 package ru.job4j.live;
 
 public class Main {
-    @SuppressWarnings("checkstyle:EmptyBlock")
     public static void main(String[] args) {  // psvm
-        for (int i = 0; i < 1; i++) {
-            System.out.println();  // sout// fori
+        for (int i = 0; i < 1; i++) {  // fori
+            System.out.println();  // sout
         }
 
     }
