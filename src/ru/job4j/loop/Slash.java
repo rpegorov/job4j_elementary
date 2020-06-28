b05cdc40645c260808b5e74c869deca98ccbffe0
rpegorov <rostislav.egorov@gmail.com>
2020-06-28 19:10:41 +0300

    Крест в псевдографике. [177182#286491]

---
+++ src/ru/job4j/loop/Slash.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell; // условие постановки символа (по графику х у (сверху вниз))
+                boolean right = (cell == (size - row - 1)); // условие постановки символа (правый график по осик у х (снизу вверх))
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
