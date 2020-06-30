ee26cdbb97fb8e7ec44ed11139e12d814af76bc8
rpegorov <rostislav.egorov@gmail.com>
2020-06-30 14:20:15 +0300

    Объявление массива.[3613#286434]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+/*
+Массивы
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        // тип_данных[] имя_переменной = new тип_данных[количество_ячеек]
+        short[] ages = new short[10];
+        String[] surname = new String[100500];
+        float[] prices = new float[40];
+
+
+    }
+}
