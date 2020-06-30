169d1ed3ea5cdbf94cd13230e89dd57393bd0f75
rpegorov <rostislav.egorov@gmail.com>
2020-06-30 14:43:52 +0300

    Заполнение массива.[226300#286426]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,28 @@ package ru.job4j.array;
 
 /*
 Массивы
+
  */
 public class ArrayDefinition {
     public static void main(String[] args) {
         // тип_данных[] имя_переменной = new тип_данных[количество_ячеек]
-        short[] ages = new short[10];
-        String[] surname = new String[100500];
-        float[] prices = new float[40];
+
+//        String[] names = new String[4];  // объявили и создали массив, выделили в памяти 4 ячейки
+              String[] names = {"Sergey", "Rostislav", "Nikolay", "Viktor"}; // еще вариант написания
+
+        // заполняем массив данными
+//        names[0] = "Sergey";
+//        names[1] = "Rostislav";
+//        names[2] = "Nikolay";
+//        names[3] = "Viktor";
+
+        // вывод массива через цикл for
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+
+        }
+//        System.out.println(names[0]);  // первый вариант
+
 
 
     }
