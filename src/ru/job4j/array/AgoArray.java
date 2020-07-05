9c63a3d71efb6ee72954b4dbceab2677a14c7771
rpegorov <rostislav.egorov@gmail.com>
2020-07-05 12:43:20 +0300

    Упорядочить массив. [#286435]

---
+++ src/ru/job4j/array/AgoArray.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+/**
+ * Метод сортировки масива по возрастанию (Selection sort)
+ * @author rpegorov
+ * @since 05.07.2020
+ */
+
+public class AgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+//        int temp = array[0];  // переменная для временного хранения значение ячейки с индексом 0
+//        array[0] = array[3];  // записываем в ячейку с индексом 0 значение ячейки с индексом 3
+//        array[3] = temp;  // записываем в ячейку с индексом 3 значение временной переменной
+        for (int i = 0; i < array.length; i++) {
+            /* предполагаем, что первый элемент является минимальным */
+            int min = array[i];
+            int min_i = i;
+            /* ищем элемент, который меньше предположенного минимума */
+            for (int j = i + 1; j < array.length ; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    min_i = j;
+                }
+
+            }
+            /* если нашелся меньший элемент, меняем их местами */
+            if (i != min_i) {
+                int temp = array[i];
+                array[i] = array[min_i];
+                array[min_i] = temp;
+            }
+        }
+
+        for (int value : array) System.out.println(value);
+    }
+}
