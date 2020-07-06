a1dd84886542b4bf436ba18705ef9c022ad8d1a0
rpegorov <rostislav.egorov@gmail.com>
2020-07-06 08:49:12 +0300

    Упорядочить массив. [#286435]. CheckStyle.

--- src/ru/job4j/array/AgoArray.java
+++ src/ru/job4j/array/AgoArray.java
@@ -8,30 +8,31 @@ package ru.job4j.array;
 
 public class AgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
+        int[] array = new int[]{5, 3, 2, 1, 4};
 //        int temp = array[0];  // переменная для временного хранения значение ячейки с индексом 0
 //        array[0] = array[3];  // записываем в ячейку с индексом 0 значение ячейки с индексом 3
 //        array[3] = temp;  // записываем в ячейку с индексом 3 значение временной переменной
         for (int i = 0; i < array.length; i++) {
             /* предполагаем, что первый элемент является минимальным */
             int min = array[i];
-            int min_i = i;
+            int minI = i;
             /* ищем элемент, который меньше предположенного минимума */
-            for (int j = i + 1; j < array.length ; j++) {
+            for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < min) {
                     min = array[j];
-                    min_i = j;
+                    minI = j;
                 }
-
             }
             /* если нашелся меньший элемент, меняем их местами */
-            if (i != min_i) {
+            if (i != minI) {
                 int temp = array[i];
-                array[i] = array[min_i];
-                array[min_i] = temp;
+                array[i] = array[minI];
+                array[minI] = temp;
             }
         }
 
-        for (int value : array) System.out.println(value);
+        for (int value : array) {
+            System.out.println(value);
+        }
     }
 }
