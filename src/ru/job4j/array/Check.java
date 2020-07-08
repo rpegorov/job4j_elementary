12eca7687aaac4f9468baa87579260eb04cccfa1
rpegorov <rostislav.egorov@gmail.com>
2020-07-08 09:57:59 +0300

    Массив заполнен true или false [#286415]. Незначительные правки.

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -2,13 +2,12 @@ package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
-        boolean rsl = true;
         for (int i = 0; i < data.length - 1; i++) {
             if (data[i] != data[i + 1]) {
                 return false;
             }
         }
-       return true;
+        return true;
     }
 }
 
