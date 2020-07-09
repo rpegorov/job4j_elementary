2589552f001e616d35a0748f54db9d4644fc4633
rpegorov <rostislav.egorov@gmail.com>
2020-07-09 08:38:17 +0300

    Слово начинается с ... [#286414]. Изменил цикл, поменялась логика.

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,9 +2,9 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startWith(char[] word, char[] pref) {
-        for (int i = 0; i < word.length - 1; i++) {
+        for (int i = 0; i < pref.length; i++) {
 //            for (int j = 0; j < pref.length - 1; j++) {
-            if (word[i] == pref[pref.length - 1]) {
+            if (word[pref.length] == pref[i]) {
                 return true;
             }
         }
--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        for (char c : word) {
-            if (c == post[post.length - 1]) {
+        for (int i = 0; i < word.length; i++) {
+            if (word[i] == post[post.length - 1]) {
                 return true;
             }
         }
