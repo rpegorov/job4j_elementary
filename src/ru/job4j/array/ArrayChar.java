88b09e1ba8af5c2121e59eb99b872bae9aadc762
rpegorov <rostislav.egorov@gmail.com>
2020-07-12 10:40:24 +0300

    Слово начинается с ... [#286414]. Исправленный.

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,13 +2,15 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startWith(char[] word, char[] pref) {
+        boolean result = true;
         for (int i = 0; i < pref.length; i++) {
 //            for (int j = 0; j < pref.length - 1; j++) {
-            if (word[pref.length] == pref[i]) {
-                return true;
+            if (word[i] != pref[i]) {
+                result =  false;
+                break;
             }
         }
-        return false;
+        return result;
     }
 
 }
