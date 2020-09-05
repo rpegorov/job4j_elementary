59583b8491f24d0c8be60c5d803a7820d48a7287
rpegorov <rostislav.egorov@gmail.com>
2020-09-05 09:49:30 +0300

    Сравнение двух массивов, тестовое

---
+++ src/ru/job4j/array/Matching.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * сравнение двух массивов и вывод одинаковых значений
+ */
+
+public class Matching {
+    public static void main(String[] args) {
+        int[] left = new int[] {1, 2, 3, 5, 6};
+        int[] right = new int[] {3, 1, 4, 7, 6};
+        for (int tmp : left) {
+            for (int i : right) {
+                if (tmp == i) {
+                    System.out.print(tmp);
+                    break;
+                }
+            }
+        }
+    }
+
+}
