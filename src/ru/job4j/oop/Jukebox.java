bf0344317b8e14520b7c9962a880841f6793a015
rpegorov <rostislav.egorov@gmail.com>
2020-09-09 13:12:17 +0300

    1.3 Вызов метода c аргументами

---
+++ src/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+
+public class Jukebox {
+    public void music(int position) {
+        String song;
+        if (position == 1) {
+            song = "Пусть бегут неуклюже...";
+        }
+        else if (position == 2) {
+            song = "Спокойной ночи...";
+        }
+        else{
+            song = "Песня не найдена!";
+        }
+        System.out.println(song);
+    }
+
+    public static void main(String[] args) {
+        Jukebox artist = new Jukebox();
+        artist.music(3);
+    }
+}
