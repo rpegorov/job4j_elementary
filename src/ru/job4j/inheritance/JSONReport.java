13f198ef7753d67393c25e3902c13255edead86e
rpegorov <rostislav.egorov@gmail.com>
2020-09-30 11:13:51 +0300

    Переопределение [#286592] v.2

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,9 +1,13 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
-        return  "Name: " + name +
+        return  "{" + System.lineSeparator() +
+                "Name: " + name + "," +
                 System.lineSeparator() +
-                "Body: " + body;
+                "Body: " + body + System.lineSeparator() +
+                "}"
+                ;
     }
 }
