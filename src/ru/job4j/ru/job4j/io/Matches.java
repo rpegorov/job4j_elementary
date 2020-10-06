2662fcc801ea5acd11a3972ef5f894aa4c255762
rpegorov <rostislav.egorov@gmail.com>
2020-10-06 21:09:15 +0300

    Scanner и чтение числа из консоли. [#286537]. Определение победителя и добавлены условия ограничения

--- src/ru/job4j/ru/job4j/io/Matches.java
+++ src/ru/job4j/ru/job4j/io/Matches.java
@@ -9,23 +9,31 @@ import java.util.Scanner;
  */
 public class Matches {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Игра 11. " +
                 "Смысл игры в следующем. На столе лежат 11 спичек. " + "\n" +
                 "Два игрока по очереди берут от 1 до 3 спичек. Выигрывает тот, " + "\n" +
                 "кто забрал последние спички.");
-        System.out.println("Ход первого игрока, укажите количество спичек - ");
-        Scanner input = new Scanner(System.in);
+        System.out.println();
+        System.out.println();
         boolean run = true;
         int matches = 11;
+        int gamer = 0;
         while (run) {
+            System.out.println("Игрок делает ход.");
             int select = Integer.valueOf(input.nextLine());
             if (matches == 0) {
                 System.out.println("Игра завершена.");
                 run = false;
+                if (gamer % 2 == 0) {
+                    System.out.println("Выиграл Игрок 1.");
+                } else
+                    System.out.println("Выиграл игрок 2.");
+
             } else if (select == 1) {
                 matches = matches - select;
                 System.out.println("Игрок убрал " + select +
-                        " спички." + "На столе осталось: " + matches + " спичек.");
+                        " спички." + "На столе осталось: " + matches);
                 System.out.println("Ход переходит к следуюшему игроку." + "\n" +
                         "Какое количество спичек желаете убрать - ");
             } else if (select == 2) {
@@ -43,7 +51,11 @@ public class Matches {
             } else if (select == 0) {
                 System.out.println("Недопустимый ход, укажите значение от 1 до 3." + "\n" +
                         "Какое количество спичек желаете убрать - ");
+            } else if (select > 3) {
+                System.out.println("Недопустимый ход, укажите значение от 1 до 3." + "\n" +
+                        "Какое количество спичек желаете убрать - ");
             }
+            gamer++;
         }
     }
 }
