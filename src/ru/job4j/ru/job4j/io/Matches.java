4d94f6c65c1896b38c57ba86ef0f5593622ec03b
rpegorov <rostislav.egorov@gmail.com>
2020-10-08 04:01:35 +0300

    Scanner и чтение числа из консоли. [#286537]. Refactoring

--- src/ru/job4j/ru/job4j/io/Matches.java
+++ src/ru/job4j/ru/job4j/io/Matches.java
@@ -8,54 +8,61 @@ import java.util.Scanner;
  * @since 06.10.2020
  */
 public class Matches {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    /** метод определения победителя */
+    private static String winner() {
+        String winner = new String();
+        for (int i = 0; i < 11; i++) {
+            int j = i++;
+            if (j % 2 == 0) {
+                winner = "Выиграл Игрок 1.";
+            } else
+                winner = "Выиграл игрок 2.";
+        }
+        System.out.println(winner);
+        return winner;
+    }
+/** метод вывода приветствия */
+    private void hello() {
         System.out.println("Игра 11. " +
                 "Смысл игры в следующем. На столе лежат 11 спичек. " + "\n" +
                 "Два игрока по очереди берут от 1 до 3 спичек. Выигрывает тот, " + "\n" +
                 "кто забрал последние спички.");
-        System.out.println();
-        System.out.println();
+    }
+/** основная логика игры */
+    public void game(Scanner input) {
         boolean run = true;
+        hello();
         int matches = 11;
-        int gamer = 0;
+        System.out.println("Игра началась. Сделайте ход.");
         while (run) {
-            System.out.println("Игрок делает ход.");
             int select = Integer.valueOf(input.nextLine());
             if (matches == 0) {
                 System.out.println("Игра завершена.");
+                winner();
                 run = false;
-                if (gamer % 2 == 0) {
-                    System.out.println("Выиграл Игрок 1.");
-                } else
-                    System.out.println("Выиграл игрок 2.");
-
-            } else if (select == 1) {
-                matches = matches - select;
-                System.out.println("Игрок убрал " + select +
-                        " спички." + "На столе осталось: " + matches);
-                System.out.println("Ход переходит к следуюшему игроку." + "\n" +
-                        "Какое количество спичек желаете убрать - ");
-            } else if (select == 2) {
-                matches = matches - select;
-                System.out.println("Игрок убрал " + select +
-                        " спички." + "На столе осталось: " + matches + " спичек.");
-                System.out.println("Ход переходит к следуюшему игроку." + "\n" +
-                        "Какое количество спичек желаете убрать - ");
-            } else if (select == 3) {
-                matches = matches - select;
-                System.out.println("Игрок убрал " + select +
-                        " спички." + "На столе осталось: " + matches + " спичек.");
-                System.out.println("Ход переходит к следуюшему игроку." + "\n" +
-                        "Какое количество спичек желаете убрать - ");
-            } else if (select == 0) {
-                System.out.println("Недопустимый ход, укажите значение от 1 до 3." + "\n" +
-                        "Какое количество спичек желаете убрать - ");
-            } else if (select > 3) {
-                System.out.println("Недопустимый ход, укажите значение от 1 до 3." + "\n" +
-                        "Какое количество спичек желаете убрать - ");
             }
-            gamer++;
+                if (select < 1 || select > 3) {
+                    System.out.println("Недопустимый ход. Укажите значение от 1 до 3... ");
+                }
+                if (select == 1) {
+                    matches = matches - select;
+                    System.out.println("Игрок убрал " + select +
+                            " спички." + "На столе осталось: " + matches);
+                } else if (select == 2) {
+                    matches = matches - select;
+                    System.out.println("Игрок убрал " + select +
+                            " спички." + "На столе осталось: " + matches);
+                } else if (select == 3) {
+                    matches = matches - select;
+                    System.out.println("Игрок убрал " + select +
+                            " спички." + "На столе осталось: " + matches);
+                }
+
+            }
         }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        new Matches().game(input);
     }
 }
