c62b261d47bcfb4d6a4f0c3746e75bfa41155dbf
rpegorov <rostislav.egorov@gmail.com>
2020-11-04 12:56:28 +0300

    0. Что такое исключение. [#286562]

--- src/ru/job4j/ex/BlackArray.java
+++ src/ru/job4j/ex/BlackArray.java
@@ -8,8 +8,8 @@ public class BlackArray {
         int middle = names.length / 2;
         for (int index = 0; index <= middle; index++) {
             String temp = names[index];
-            names[index++] = names[names.length - index];
-            names[names.length - index] = temp;
+            names[index] = names[names.length - index - 1];
+            names[names.length - index - 1] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
