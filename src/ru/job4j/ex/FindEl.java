33ef6e4fc265d4c9216cc3a551a9ec283f292ff5
rpegorov <rostislav.egorov@gmail.com>
2020-11-05 09:54:40 +0300

    Пользовательские исключения. [#286557] добавлен break

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -6,9 +6,12 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
-        if (rsl == -1) throw new ElementNotFoundException();
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
+        }
         return rsl;
     }
 
